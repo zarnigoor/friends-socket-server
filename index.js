8e12f4f0348afc01e237d098b4a5e8d9d5995add
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-07-25 13:33:29 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import { Server } from "socket.io"
+import { createServer } from "http"
+
+const users = []
+
+const httpServer = createServer()
+const io = new Server( httpServer, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ],
+	}
+} )
+
+httpServer.listen( 3_000, () => {
+
+	console.log( "Server listening on port 3000" )
+} )
+
+io.on( "connection", user => {
+
+	users.push( user )
+
+	user.on( "new_user", user => {
+
+		const geoJSON = {
+			type: "Feature",
+			properties: {
+				username: user.username,
+				avatar: user.avatar,
+			},
+			geometry: {
+				type: "Point",
+				coordinates: user.coordinates,
+			}
+		}
+
+		for ( const user of users ) {
+
+			user.emit( "new_user", geoJSON )
+		}
+	} )
+
+	console.log( "New user..." )
+} )
